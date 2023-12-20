879e284cc15af0375549e205a64c9de13fe7ac04
batalovM <b.maksim1708@gmail.com>
2023-12-20 23:01:43 +0700

    add

---
+++ FerryCrossing/Models/Classes/Ferry.cs
@@ -0,0 +1,8 @@
+﻿namespace FerryCrossing.Models.Classes;
+
+public class Ferry
+{
+    public const int CapacityPerson = 50;
+    public const int CapacityCar = 20;
+    public const int CapacityTruck = 10;
+}
